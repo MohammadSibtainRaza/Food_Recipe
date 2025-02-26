ad4a4c4acb601c4164fa246d5a4763a5306be2ed
MohammadSibtainRaza <143188061+MohammadSibtainRaza@users.noreply.github.com>
2025-02-26 19:08:02 +0530

    Update app.py
    The use_column_width parameter has been deprecated and will be removed in a future release. so I utilize the use_container_width parameter instead.

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ uploaded_file = st.file_uploader("Upload a food image", type=["jpg", "jpeg", "pn
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
     model, processor = load_model()
     food_item = classify_food(image, model, processor)
     st.write(f"Classified Food Item: **{food_item}**")
